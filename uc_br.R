a9a007895fb907c1b5344a55e1a6b7ac53ad9a75
aisneto <neto.sodre@gmail.com>
2022-05-31 10:51:53 -0300

    Estabelece as funções "list.uc()" e "read.uc()"

--- uc_br.R
+++ uc_br.R
@@ -15,3 +15,21 @@ unzip(file, exdir = dir)
 list.files(dir)
 setwd(dir)
 x <- st_read("GEOFT_UNIDADE_CONSERVACAO.shp")
+
+# Lista as unidades de conservação de acordo com o ID
+
+list.uc <- function() {
+  a <- data.frame(x$NOME_UC1, x$ID_UC0)
+  colnames(a) <- c("Nome", "ID")
+  print(a)
+  
+}
+
+# Acessa o smart object com base no id
+
+read.uc <- function(id) {
+  x$geometry[x$ID_UC0 == id ]
+}
+
+
+
