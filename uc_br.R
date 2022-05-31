47a19e74921eb1ccdbe323164ee93b721c46dd69
aisneto <neto.sodre@gmail.com>
2022-05-31 14:39:22 -0300

    Incorpora o download dos dados nas funções com um argumento lógico para evitar downloads repetidos.

--- uc_br.R
+++ uc_br.R
@@ -1,19 +1,24 @@
-###################################### Projeto de package "uc_br()"###################################
-########################################Desenvolvido por aisneto #####################################
+########################################## Projeto de package "uc_br()"###########################################
+########################################### Desenvolvido por aisneto #############################################
 
 
 #Cria diretorio temporario e faz download do shapefile
-file <- tempfile()
-dir <- tempdir()
-download.file("https://metadados.snirh.gov.br/geonetwork/srv/api/records/9407d38f-84d2-48ea-97dd-ee152c493043/attachments/GEOFT_UNIDADE_CONSERVACAO.zip",
-                       destfile = file, quiet = T)
-unzip(file, exdir = dir)
-uc_br <- sf::st_read(paste(dir,"\\GEOFT_UNIDADE_CONSERVACAO.shp", sep = ""))
-
+download.data <- function() {
+  file <- tempfile()
+  tempdir <- tempdir()
+  download.file("https://metadados.snirh.gov.br/geonetwork/srv/api/records/9407d38f-84d2-48ea-97dd-ee152c493043/attachments/GEOFT_UNIDADE_CONSERVACAO.zip",
+                destfile = file)
+  unzip(file, exdir = tempdir)
+}
 
 # Lista as unidades de conservação de acordo com o ID
 list.uc <- function() {
-  a <- data.frame(x$NOME_UC1, x$ID_UC0)
+  tempdir <- tempdir()
+  if (file.exists(paste(tempdir,"\\GEOFT_UNIDADE_CONSERVACAO.shp", sep = "")) == F) {
+    uc_br <- download.data()
+  }
+  uc_br <- sf::st_read(paste(tempdir,"\\GEOFT_UNIDADE_CONSERVACAO.shp", sep = ""))
+  a <- data.frame(uc_br$NOME_UC1, uc_br$ID_UC0)
   colnames(a) <- c("Nome", "ID")
   print(a)
   
@@ -21,7 +26,10 @@ list.uc <- function() {
 
 # Acessa o smart object com base no id
 read.uc <- function(id) {
-  x$geometry[x$ID_UC0 == id ]
+  tempdir <- tempdir()
+  if (file.exists(paste(tempdir,"\\GEOFT_UNIDADE_CONSERVACAO.shp", sep = "")) == F) {
+    uc_br <- download.data()
+  }
+  uc_br <- sf::st_read(paste(tempdir,"\\GEOFT_UNIDADE_CONSERVACAO.shp", sep = ""))
+  uc_br$geometry[uc_br$ID_UC0 == id ]
 }
-
-
