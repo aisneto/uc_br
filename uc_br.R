081142c1dabdc9c835460ea7f274892c9caaccf7
aisneto <neto.sodre@gmail.com>
2022-05-31 13:33:57 -0300

    Deixa o código mais limpo.

--- uc_br.R
+++ uc_br.R
@@ -1,23 +1,17 @@
 ###################################### Projeto de package "uc_br()"###################################
 ########################################Desenvolvido por aisneto #####################################
 
-#Carrega packages necessárias
-library(sf)
-install.packages("sf")
 
 #Cria diretorio temporario e faz download do shapefile
-
 file <- tempfile()
 dir <- tempdir()
-teste <- download.file("https://metadados.snirh.gov.br/geonetwork/srv/api/records/9407d38f-84d2-48ea-97dd-ee152c493043/attachments/GEOFT_UNIDADE_CONSERVACAO.zip",
-                       destfile = file)
+download.file("https://metadados.snirh.gov.br/geonetwork/srv/api/records/9407d38f-84d2-48ea-97dd-ee152c493043/attachments/GEOFT_UNIDADE_CONSERVACAO.zip",
+                       destfile = file, quiet = T)
 unzip(file, exdir = dir)
-list.files(dir)
-setwd(dir)
-x <- st_read("GEOFT_UNIDADE_CONSERVACAO.shp")
+uc_br <- sf::st_read(paste(dir,"\\GEOFT_UNIDADE_CONSERVACAO.shp", sep = ""))
+
 
 # Lista as unidades de conservação de acordo com o ID
-
 list.uc <- function() {
   a <- data.frame(x$NOME_UC1, x$ID_UC0)
   colnames(a) <- c("Nome", "ID")
@@ -26,10 +20,8 @@ list.uc <- function() {
 }
 
 # Acessa o smart object com base no id
-
 read.uc <- function(id) {
   x$geometry[x$ID_UC0 == id ]
 }
-
 
 
